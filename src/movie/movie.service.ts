b8d7b768e6aaa107b9f7884f3e372af34e92bc21
auddnjs2008 <auddnjs2008@naver.com>
2025-05-23 07:41:42 +0900

    setting & 환경변수

---
+++ src/movie/movie.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entity/movie.entity';
+
+@Injectable()
+export class MovieService {
+  private movies: Movie[] = [];
+
+  private idCounter = 3;
+
+  constructor() {
+    const movie1 = new Movie();
+    movie1.id = 1;
+    movie1.title = 'harry potter';
+    movie1.genre = 'fantasy';
+
+    const movie2 = new Movie();
+    movie2.id = 2;
+    movie2.title = 'harry potter';
+    movie2.genre = 'fantasy';
+
+    this.movies.push(movie1, movie2);
+  }
+
+  getManyMovies(title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+    return this.movies.filter((m) => m.title.startsWith(title));
+  }
+
+  getMovieById(id: number) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
+  }
+
+  createMovie(createMovieDto: CreateMovieDto) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      ...createMovieDto,
+    };
+    this.movies.push(movie);
+    return movie;
+  }
+
+  updateMovie(id: number, updateMovieDto: UpdateMovieDto) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    Object.assign(movie, { ...updateMovieDto });
+    return movie;
+  }
+
+  deleteMovie(id: number) {
+    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
+    if (movieIndex === -1) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    this.movies.splice(movieIndex, 1);
+
+    return id;
+  }
+}
